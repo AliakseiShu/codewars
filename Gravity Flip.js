a3f5d40dded3cc26b12e3b7a439e18e40a457118
Alex <aliakseishulha2021@gmail.com>
2022-12-17 20:45:15 +0300

    I did task Gravity Flip

---
+++ Gravity Flip.js
@@ -0,0 +1,9 @@
+const flip=(d, a)=>{
+    if(d === 'R') {
+        return a.sort(function(a, b){
+            return a-b
+        })}
+    return a.sort(function(a, b){
+        return a-b
+    }).reverse()
+}

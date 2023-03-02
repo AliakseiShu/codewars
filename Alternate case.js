d76f3e1bf63484b1c52674c304b1557f0a9333da
Alex <aliakseishulha2021@gmail.com>
2023-03-02 12:26:10 +0300

    Alternate case

---
+++ Alternate case.js
@@ -0,0 +1,8 @@
+function alternateCase(s) {
+    return s
+        .split('')
+        .map((el) =>
+            el === el.toUpperCase() ? el.toLowerCase() : el.toUpperCase()
+        )
+        .join('')
+}
